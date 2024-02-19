795a2965893bdd84b6e6b0222917f4ca1aa22710
imPradhyumn <pradhyumn.16jics040@jietjodhpur.ac.in>
2024-02-20 01:20:24 +0530

    @Annotatoins added

--- src/main/java/com/spotifyclone/SpotifyCloneApplication.java
+++ src/main/java/com/spotifyclone/SpotifyCloneApplication.java
@@ -2,10 +2,14 @@ package com.spotifyclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.spotifyclone.entities")
+@EnableJpaRepositories("com.spotifyclone.*")
+@ComponentScan(basePackages = {"com.spotifyclone.*"})
 public class SpotifyCloneApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpotifyCloneApplication.class, args);
