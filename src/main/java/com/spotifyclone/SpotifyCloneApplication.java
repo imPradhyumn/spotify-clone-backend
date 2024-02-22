3fe64728bf2330a63dc621159e23395df1dbe3d2
imPradhyumn <pradhyumn.16jics040@jietjodhpur.ac.in>
2024-02-23 00:57:34 +0530

    Removed enableautocongif and componentscan

--- src/main/java/com/spotifyclone/SpotifyCloneApplication.java
+++ src/main/java/com/spotifyclone/SpotifyCloneApplication.java
@@ -2,16 +2,13 @@ package com.spotifyclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
-@EntityScan("com.spotifyclone.entities")
-@EnableJpaRepositories("com.spotifyclone.repositories")
-@ComponentScan(basePackages = {"com.spotifyclone.services"})
 public class SpotifyCloneApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpotifyCloneApplication.class, args);
+
     }
 }
